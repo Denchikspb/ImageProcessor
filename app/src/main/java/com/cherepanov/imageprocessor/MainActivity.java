4784cf32f5cb8ddb7eb147a0f892ed34bec2923e
Denis <cherepanov.denis94@mail.ru>
2017-11-21 17:02:58 +0300

    add runtime permissions

--- app/src/main/java/com/cherepanov/imageprocessor/MainActivity.java
+++ app/src/main/java/com/cherepanov/imageprocessor/MainActivity.java
@@ -1,8 +1,16 @@
 package com.cherepanov.imageprocessor;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.cherepanov.imageprocessor.view.listImage.ListImageFragment;
 import com.cherepanov.imageprocessor.view.processor.ImageProcessFragment;
@@ -11,11 +19,16 @@ public class MainActivity extends AppCompatActivity implements
         ImageProcessFragment.OnFragmentInteractionListener,
         ListImageFragment.OnListInteractionListener {
 
+    private static final int PERMISSION_REQUEST_CODE = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!hasPermissions()){
+            requestPermissionWithRationale();
+        }
     }
 
     @Override
@@ -33,6 +46,58 @@ public class MainActivity extends AppCompatActivity implements
                 .findFragmentById(R.id.image_fragment);
         if (fragment != null && fragment.isInLayout()) {
             fragment.addNewSrcImage(bitmap);
+        }
+    }
+
+    public boolean hasPermissions(){
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPerms(){
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissions,PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public void requestMultiplePermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                showExtDirFilesCount();
+            }
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+//                showUnreadSmsCount();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void requestPermissionWithRationale() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            final String message = "Storage permission is needed to show images";
+            Snackbar.make(MainActivity.this.findViewById(R.id.main_activity), message, Snackbar.LENGTH_LONG)
+                    .setAction("GRANT", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            requestPerms();
+                        }
+                    })
+                    .show();
+        } else {
+            requestPerms();
         }
     }
 }
